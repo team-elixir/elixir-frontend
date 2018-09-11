76daceb01d4f3642dbf25b5dc04acf4a64958df2
Chaudhary <723311@1000810002446M.local>
2018-09-11 11:12:04 +1000

    Added some changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './_components/bootstrap/dist/css/bootstrap.css';
+import './_components/bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './_componenets/Navigation';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Navigation />, document.getElementById('root'));
 registerServiceWorker();
