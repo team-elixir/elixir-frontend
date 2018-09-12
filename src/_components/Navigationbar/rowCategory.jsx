5c0ed5d53ee2eff390148f5c9d43770b36e67a76
jamechet <rabou.von@gmail.com>
2018-09-12 16:41:03 +1000

    Row Category

---
+++ src/_components/Navigationbar/rowCategory.jsx
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import { Button, ButtonGroup } from 'reactstrap';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+class RowCategory extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            dropdownOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState(prevState => ({
+            dropdownOpen: !prevState.dropdownOpen
+        }));
+    }
+    styles = {
+
+        borderRadius: 50,
+        width: 200
+
+    };
+    render() {
+        return (
+            <div className="text-center m-3">
+                    <ButtonGroup className="d-flex justify-content-center">
+                        <Dropdown  isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                            <DropdownToggle caret className="m-2" style={{borderRadius: 50, width: 200}}>
+                                Shop by Category
+                            </DropdownToggle>
+                            <DropdownMenu   >
+                                <DropdownItem>Electronics</DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem>Clothes</DropdownItem>
+                            </DropdownMenu>
+                        </Dropdown>
+
+                        <Button style={this.styles} className= " btn btn-outline-info m-2">Electronics</Button>
+                        <Button style={this.styles} className= " btn-outline-info m-2">Home Appliances</Button>
+                        <Button style={this.styles} className= " btn-outline-info m-2">Music Accessories</Button>
+                        <Button style={this.styles} className= " btn-outline-info m-2">Apparel</Button>
+                        <Button style={this.styles} className= " btn-outline-info m-2">Footwear</Button>
+                    </ButtonGroup>
+            </div>
+        );
+    }
+}
+
+export default RowCategory;
