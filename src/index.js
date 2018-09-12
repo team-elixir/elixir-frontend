e432ae434b055012d37cc0b52bb967605a41b488
Chaudhary <723311@1000810002446M.local>
2018-09-12 15:38:14 +1000

    Navbar for homepage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './bootstrap/css/bootstrap.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Navigationbar from "./_components/Navigationbar/Navigationbar";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Navigationbar />, document.getElementById('root'));
 registerServiceWorker();
