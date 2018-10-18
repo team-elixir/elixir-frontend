0f03d227baf894fc9ba4e28a09cd43e46d5b36a1
yangyu823 <yangyu823@gmail.com>
2018-10-18 11:13:44 +1100

    footer wrap

--- src/_components/Home/Footer.jsx
+++ src/_components/Home/Footer.jsx
@@ -7,7 +7,7 @@ class Footer extends Component {
             <section id="footer">
                 <div className="container-flux">
                     <div className="row text-center text-xs-center text-sm-left text-md-left" >
-                        <div className="col-md-2 pl-md-5">
+                        <div className="col-md-3 pl-md-5">
                             <h5>ABOUT GRIZZLY</h5>
                             <ul className="list-unstyled quick-links">
                                 <li><a href="https://elixir.ausgrads.academy"><i className="fa fa-angle-double-right"/>About
@@ -22,7 +22,7 @@ class Footer extends Component {
                             </ul>
                         </div>
                         <div className="col-md-3 pl-md-5">
-                            <h5>CUSTOMER SERVICE</h5>
+                            <h5>ABOUT SERVICE</h5>
                             <ul className="list-unstyled quick-links">
                                 <li><a href="https://elixir.ausgrads.academy"><i className="fa fa-angle-double-right"/>Contact
                                     Us</a></li>
@@ -30,26 +30,24 @@ class Footer extends Component {
                                     Policy</a></li>
                                 <li><a href="https://elixir.ausgrads.academy"><i className="fa fa-angle-double-right"/>Shipping
                                     Policies</a></li>
-                                <li><a href="https://elixir.ausgrads.academy"><i className="fa fa-angle-double-right"/>Order
-                                    Status</a></li>
                                 <li><a href="https://elixir.ausgrads.academy"><i className="fa fa-angle-double-right"/>FAQ</a>
                                 </li>
                             </ul>
                         </div>
                         <div className="col-md-3 pl-md-2">
-                            <h2> NEED HELP?</h2>
+                            <h4 id='needhelp'> NEED HELP?</h4>
                             <br/>
-                            <h4>ALWAYS LOOKING TO IMPROVE</h4>
+                            {/*<h5>ALWAYS LOOKING TO IMPROVE</h5>*/}
                             <a>Tell us what you love or what we need to fix.</a>
                             <br/>
                             <br/>
-                            <button style={{borderRadius: 10, width: 260, height: 50}}>Leave Feedback.</button>
+                            <button style={{borderRadius: 10, width: 200, height: 50}}>Leave Feedback.</button>
                         </div>
-                        <div className="col-md-4 pl-md-3">
-                            <iframe title='Location' width={400} height='100%' frameBorder={0} style={{border: 0}}
+                        <div className="col-md-3 pl-md-3">
+                            <iframe title='Location' width='100%' height='90%' frameBorder={0} style={{border: 0}}
                                     src="https://www.google.com/maps/embed/v1/view?key=AIzaSyAVk4NSVeHTQ1yU7yBpbku5ezU72raWJNQ
                                     &center=-37.818861, 144.959294
-                                    &zoom=18
+                                    &zoom=12
                                     &maptype=roadmap"
                                     //&maptype=satellite"
                                     allowFullScreen>
