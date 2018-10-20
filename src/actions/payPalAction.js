7458f56c41526c8486b8e94e6b9db61d7f3aa8e2
jamechet <rabou.von@gmail.com>
2018-10-20 23:08:01 +1100

    updateThirdPartyv1

--- src/actions/payPalAction.js
+++ src/actions/payPalAction.js
@@ -25,3 +25,6 @@ export const payment = (sum) => dispatch => {
 
 };
 
+
+
+
--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,9 +1,12 @@
 import {ADD_TO_CART, DELETE_PRODUCT_FROM_CART, FETCH_CART_FULLFILLED} from "../actions/types";
 
 const initialState = {
-  cart: [],
-  msgFromServer: "",
-  deletemsgFromServer: ""
+    cart: [],
+    cartPending:'',
+    cartComplete:[],
+    orderStatus:'',
+    msgFromServer: "",
+    deletemsgFromServer: ""
 };
 
 export default function(state = initialState, action) {
@@ -23,6 +26,22 @@ export default function(state = initialState, action) {
               ...state ,
               deletemsgFromServer: action.payload
           };
+      case 'ORDER-STATUS-COMPLETE':
+          return {
+              ...state ,
+              cartComplete: action.payload
+          };
+      case 'ORDER-STATUS-PENDING':
+          return {
+              ...state ,
+              cartPending: action.payload
+          };
+      case 'UPDATE-ORDER-STATUS':
+          return {
+              ...state ,
+              orderStatus: action.payload
+          };
+
     default:
       return state;
   }
